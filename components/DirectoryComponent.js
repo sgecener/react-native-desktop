89c349c26962f343be707633d3a2b4b7d697c990
Saygin Gecener <sgecener@yahoo.com>
2021-09-04 01:25:42 -0500

    Modal

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { View, FlatList, ViewPropTypes } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+
 
 const mapStateToProps = state => {
     return {
@@ -28,6 +30,18 @@ class Directory extends Component {
                     onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
                     imageSrc={{uri: baseUrl + item.image}}
                 />
+            );
+        };
+
+        if (this.props.campsites.isLoading) {
+            return <Loading />;
+        };
+
+        if (this.props.campsites.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.campsites.errMess}</Text>
+                </View>
             );
         };
 
